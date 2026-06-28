365a4b22c40353908b372ad21dfc5713375c85cb
Yomesh Nagar <yomeshnagar2006@gmail.com>
2026-06-28 11:45:29 +0530

    Doc Verse

--- Frontend/src/pages/Doctors.jsx
+++ Frontend/src/pages/Doctors.jsx
@@ -162,7 +162,7 @@ const Doctors = () => {
           </div>
         </div>
 
-        <div className="w-[90%] sm:w-[500px] md:w-[700px] lg:w-[928px] h-px bg-gray-600 mx-auto relative top-[64px]"></div>
+        <div className="w-[90%] sm:w-[500px] md:w-[700px] lg:w-[928px] h-px bg-gray-600 mx-auto relative top-[66px]"></div>
       </section>
 
       <Footer />
